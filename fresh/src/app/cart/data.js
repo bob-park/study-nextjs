c10d00bd2a5e0bbeca6da6ee7c15345ea19fecb6
Bob Park <hwpark11@kakao.com>
2023-07-26 23:34:45 +0900

    feat: client / server component 사용법

---
+++ fresh/src/app/cart/data.js
@@ -0,0 +1,1 @@
+export let age = 20; // 다른 js 에서 사용하고 싶은 경우 반드시 export 해야함
--- fresh/src/app/cart/page.tsx
+++ fresh/src/app/cart/page.tsx
@@ -1,10 +1,39 @@
 import CartItem from '@/components/CartItem';
 
-// homework
+import { age } from './data';
+
+/**
+ *
+ * * component 는 재사용이 잦은 UI 를 component 로 만든다.
+ *
+ * 1. 더러운 코드 축약
+ * 2. 같은 코드 재사용
+ *
+ * * nextjs 는 컴포넌트 2개
+ *
+ * server component
+ * - 그냥 nextjs 에서 component 만들면 server compoent
+ * - html 에 (return(..)) 에 js 기능 넣기 불가능
+ * - html 를 동적으로 바꿀수 없음 (useEffect 머 이런거) => 진짜 안되네
+ * - 로딩 속도가 빠름
+ * - 검색엔진 노출 가능
+ * - 큰 페이지는 server component 로 만드는 걸 추천
+ *
+ * client component
+ * - 맨위에 'use client' 추가하면 client component
+ * - html 에 (return(..)) 에 js 기능 넣기 가능
+ * - html 동적으로 바꿀 수 있음 (useEffect, 머 이런거)
+ * - 로딩 속도 느림
+ * - 검색엔진 노출이 힘듬
+ * - hydration 필요 (client 에 js 보내주고 분석하는 작업)
+ * - js 기능만 필요한 곳
+ *
+ */
 export default function Cart() {
   return (
     <div>
-      <h4 className="font-bold text-center mt-10">Cart</h4>
+      <h4 className="font-bold text-center mt-10">Cart {age}</h4>
+      <CartItem />
       <CartItem />
     </div>
   );
