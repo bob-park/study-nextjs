f1b1186eba079c912455412fc8bb39cd3fcbf7f9
Bob Park <hwpark11@kakao.com>
2023-07-27 00:12:06 +0900

    feat: 좋아요 버튼 만들기 2 (array, object state 변경하려면) + 오늘의 숙제 완료

--- fresh/src/app/list/page.tsx
+++ fresh/src/app/list/page.tsx
@@ -1,10 +1,14 @@
-// nextjs 는 pages dir 하위 dir 이 URL 이 됨
+'use client';
 
+// nextjs 는 pages dir 하위 dir 이 URL 이 됨
 import Item from '@/components/Item';
+import { useState } from 'react';
 
 // 반드시 `export default` 를 붙여야함
 export default function List() {
   let items = ['Tomatoes', 'Pasta', 'Coconut'];
+
+  const [itemCounts, setItemCounts] = useState<number[]>([0, 0, 0]);
 
   return (
     <div>
@@ -16,7 +20,31 @@ export default function List() {
         <h4>상품2 $40</h4>
       </div> */}
       {items.map((item, index) => (
-        <Item key={item} imgSrc={`/food${index}.png`} name={item} price={40} />
+        <Item
+          key={item}
+          imgSrc={`/food${index}.png`}
+          name={item}
+          price={40}
+          count={itemCounts[index]}
+          onPlus={() =>
+            setItemCounts((prev) => {
+              const newCounts = [...prev];
+
+              newCounts[index] = prev[index] + 1;
+
+              return newCounts;
+            })
+          }
+          onMinus={() =>
+            setItemCounts((prev) => {
+              const newCounts = [...prev];
+
+              newCounts[index] = prev[index] <= 0 ? 0 : prev[index] - 1;
+
+              return newCounts;
+            })
+          }
+        />
       ))}
     </div>
   );
--- fresh/src/components/Item.tsx
+++ fresh/src/components/Item.tsx
@@ -10,6 +10,9 @@ type ItemProps = {
   imgSrc?: string;
   name: string;
   price: number;
+  count: number;
+  onPlus?: () => void;
+  onMinus?: () => void;
 };
 
 //
@@ -22,8 +25,22 @@ type ItemProps = {
  * ! 단점 - 외부 이미지 경로 넣기가 힘듬(하지만, 넣을 수 있지만, width, height 를 추가로 넣어야함) + next.config.js 에 셋팅이 필요함
  *
  * */
-export default function Item({ imgSrc, name, price }: ItemProps) {
-  const [count, setCount] = useState<number>(0);
+export default function Item({
+  imgSrc,
+  name,
+  price,
+  count,
+  onPlus,
+  onMinus,
+}: ItemProps) {
+  // handle
+  const handlePlus = () => {
+    onPlus && onPlus();
+  };
+
+  const handleMinus = () => {
+    onMinus && onMinus();
+  };
 
   return (
     <div className="mx-auto my-[20px] w-[200px] bg-white text-black p-[20px] rounded-[5px] mt-2">
@@ -42,17 +59,10 @@ export default function Item({ imgSrc, name, price }: ItemProps) {
         {name} ${price}
       </h4>
       <span> {count} </span>
-      <button className="w-5 bg-gray-300" onClick={() => setCount(count + 1)}>
+      <button className="w-5 bg-gray-300" onClick={handlePlus}>
         +
       </button>
-      <button
-        className="ml-1 w-5 bg-gray-300"
-        onClick={() =>
-          setCount((prev) => {
-            return prev <= 0 ? 0 : prev - 1;
-          })
-        }
-      >
+      <button className="ml-1 w-5 bg-gray-300" onClick={handleMinus}>
         -
       </button>
     </div>
