8a9d6d863e52eb83525a81de084121492fbb726a
Bob Park <hwpark11@kakao.com>
2023-07-26 22:54:02 +0900

    feat: map 사용ㅋ

--- fresh/src/app/list/page.tsx
+++ fresh/src/app/list/page.tsx
@@ -1,15 +1,23 @@
 // nextjs 는 pages dir 하위 dir 이 URL 이 됨
+
+import Item from '@/component/Item';
+
 // 반드시 `export default` 를 붙여야함
 export default function List() {
+  let items = ['Tomatoes', 'Pasta', 'Coconut'];
+
   return (
     <div>
       <h4 className="font-bold text-center mt-10">상품목록</h4>
-      <div className="mx-[20px] my-auto w-[200px] bg-white text-black p-[20px] rounded-[5px] mt-2">
+      {/* <div className="mx-[20px] my-auto w-[200px] bg-white text-black p-[20px] rounded-[5px] mt-2">
         <h4>상품1 $40</h4>
       </div>
       <div className="mx-[20px] my-auto w-[200px] bg-white text-black p-[20px] rounded-[5px] mt-2">
         <h4>상품2 $40</h4>
-      </div>
+      </div> */}
+      {items.map((item) => (
+        <Item key={item} name={item} price={40} />
+      ))}
     </div>
   );
 }
