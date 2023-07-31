94f83de07707a5a261007d586009c12edd7b881e
Bob Park <hwpark11@kakao.com>
2023-07-31 21:32:53 +0900

    feat: 먼가 꺠달은거 주석 추가

--- forum/src/app/list/page.tsx
+++ forum/src/app/list/page.tsx
@@ -6,6 +6,8 @@ import PostClient from './PostClient';
 /**
  *
  * ! 문제가 있네... mongodb 에서 값이 바로 적용되는데 이게 next page 에서 caching 하고 있는지 바로 적용이 안된다
+ * ! 아 알았다. 이미 server component 가 로드되어 있기 때문에, <Link /> 든 router 를 써서 페이지를 이동하면, 이미 로드된 page 를 보여주는 구만
+ * ! 이걸 해결할려면, redux + sage 를 써서 페이지 접속 시 먼가 작업을 해줘야 되는 구만
  *
  * @returns
  *
